8ad95428a7653a0e429f90593cb6f64d021b2c1e
Henrique Elias <hcbe2004@gmail.com>
2019-08-20 08:10:20 -0300

    streaming from aws

--- src/api/video/video.controller.js
+++ src/api/video/video.controller.js
@@ -69,7 +69,7 @@ function writeResponseHeader(start, end, res) {
   res.writeHead(206, head);
 }
 
-function streamFile(userId, index, res, next, req) {
+function streamFile(userId, index, req, res, next) {
 
   let range = req.headers.range;
 
@@ -79,24 +79,15 @@ function streamFile(userId, index, res, next, req) {
     return next(err);
   }
 
-  let { start, end, chunkSize } = calculateChunk(range);
-  return streamS3File(userId, index, start, end, chunkSize).then(stream => {
-    writeResponseHeader(start, end, res);
-    stream.on('open', function () {
-      debugger;
-      stream.pipe(res);
-    });
-    stream.on('error', function (err) {
-      stream.pipe(res);
-      return next(err);
-    });
-  })
+  //let { start, end, chunkSize } = calculateChunk(range);
+  streamS3File(userId, index, range, next, res);
   //writeResponseHeader(start, end, file_size, chunksize, res);
   //createStream(start, end, file, res, next);
 
 }
-async function streamS3File(userId, index, start, end, chunkSize) {
+async function streamS3File(userId, index, range, next, res) {
   const fileList = [
+    'abertura-de-caixa-sistema.mp4',
     'abertura-de-caixa-sistema.mp4',
     'abertura-de-loja.mp4',
     'credito-de-ficha-item-de-mesmo-valor.mp4'
@@ -109,13 +100,45 @@ async function streamS3File(userId, index, start, end, chunkSize) {
   });
   var s3 = new AWS.S3({ apiVersion: '2006-03-01' });
   const name = fileList[index];
-  var params = { Bucket: 'growleria-videos', Key: `${name}`, Range: `bytes=${start}-${!!end ? '-' + end : ''}` };
-  return await s3.getObject(params).createReadStream();
+  var params = { Bucket: 'growleria-videos', Key: `${name}` };
+  let metadata = await s3.headObject(params, function (err, data) {
+    if (err) {
+      // an error occurred
+      console.error(err);
+      return next();
+    }
+
+    let positions = range.replace(/bytes=/, '').split('-');
+    let start = parseInt(positions[0], 10);
+    let fileSize = data.ContentLength;
+    let end = positions[1] ? parseInt(positions[1], 10) : fileSize - 1;
+
+    Object.assign(params, { Range: `bytes=${start}-${end}` });
+    var stream = s3.getObject(params).createReadStream();
+
+    // forward errors
+    stream.on('error', function error(err) {
+      //continue to the next middlewares
+      return next();
+    });
+
+    //Add the content type to the response (it's not propagated from the S3 SDK)
+    res.set('Content-Type', 'video/mp4');
+    res.set('Content-Length', data.ContentLength);
+    res.set('Last-Modified', data.LastModified);
+    res.set('ETag', data.ETag);
+
+    stream.on('end', () => {
+      console.log('Served');
+    });
+    //Pipe the s3 object to the response
+    stream.pipe(res);
+  });
+  //return await s3.getObject(params).createReadStream();
 }
 
 export function show(req, res, next) {
-  return streamFile(req.user._id.toString(), req.params.number, res, next, req, next);
-
+  streamFile(req.user._id.toString(), req.params.number, req, res, next);
 }
 
 export function create(req, res) {
