aa6097123affde1bd8a0950fda84ed95cb756732
Henrique Elias <hcbe2004@gmail.com>
2019-08-20 17:43:32 -0300

    streaming bytes

--- src/routes.js
+++ src/routes.js
@@ -7,9 +7,6 @@
 export default function (app) {
   app.use('/api/users', require('./api/user'));
   app.use('/api/videos', require('./api/video'));
-  app.route('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
-  });
   app.route('/*').get((req, res) => {
     return res.status(404).end();
   });
